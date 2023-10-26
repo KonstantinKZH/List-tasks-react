a1c9a339b14c8cebe9a0f2941ea8f9f0d55676ec
Konstantin Kinzhitaev <boss.kinzhit@mail.ru>
2023-10-26 11:20:24 -0700

    fixed the vite.cinfig.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://konstantinkzh.github.io/List-tasks-react/"
+  base: "/konstantinkzh.github.io/List-tasks-react/"
 })
